04502173ec81ea65714dd426436417be20504e83
KAnggara75 <KAnggara75@gmail.com>
2026-04-12 23:37:54 +0700

    feat: add include_code parameter to toggle code column in history response

--- internal/handlers/history_handler.go
+++ internal/handlers/history_handler.go
@@ -61,6 +61,7 @@ func (h *HistoryHandler) SyncStockHistoryHandler(c fiber.Ctx) error {
 func (h *HistoryHandler) GetStockHistoryHandler(c fiber.Ctx) error {
 	code := c.Query("code")
 	output := c.Query("output", "json")
+	includeCode := c.Query("include_code", "false") == "true"
 
 	if code == "" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -78,23 +79,30 @@ func (h *HistoryHandler) GetStockHistoryHandler(c fiber.Ctx) error {
 
 	if output == "csv" {
 		c.Set("Content-Type", "text/csv")
-		c.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"history_%s.csv\"", strings.ToLower(code)))
+		c.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.csv\"", strings.ToUpper(code)))
 
 		writer := csv.NewWriter(c)
 		// Write header
-		header := []string{
-			"code", "date", "previous", "open_price", "first_trade", "high", "low", "close", "change",
+		header := []string{}
+		if includeCode {
+			header = append(header, "code")
+		}
+		header = append(header,
+			"date", "previous", "open_price", "first_trade", "high", "low", "close", "change",
 			"volume", "value", "frequency", "index_individual", "offer", "offer_volume", "bid", "bid_volume",
 			"listed_shares", "tradeble_shares", "weight_for_index", "foreign_sell", "foreign_buy",
 			"delisting_date", "non_regular_volume", "non_regular_value", "non_regular_frequency",
-		}
+		)
 		if err := writer.Write(header); err != nil {
 			return err
 		}
 
 		for _, row := range data {
-			line := []string{
-				row.Code,
+			line := []string{}
+			if includeCode {
+				line = append(line, row.Code)
+			}
+			line = append(line,
 				row.Date.Format("2006-01-02"),
 				formatFloat(row.Previous),
 				formatFloat(row.OpenPrice),
@@ -120,7 +128,7 @@ func (h *HistoryHandler) GetStockHistoryHandler(c fiber.Ctx) error {
 				formatFloat(row.NonRegularVolume),
 				formatFloat(row.NonRegularValue),
 				formatFloat(row.NonRegularFrequency),
-			}
+			)
 			if err := writer.Write(line); err != nil {
 				return err
 			}
@@ -128,6 +136,42 @@ func (h *HistoryHandler) GetStockHistoryHandler(c fiber.Ctx) error {
 
 		writer.Flush()
 		return nil
+	}
+
+	if !includeCode {
+		// Strip 'code' from JSON response if requested
+		results := make([]map[string]any, len(data))
+		for i, v := range data {
+			results[i] = map[string]any{
+				"date":                  v.Date,
+				"previous":              v.Previous,
+				"open_price":            v.OpenPrice,
+				"first_trade":           v.FirstTrade,
+				"high":                  v.High,
+				"low":                   v.Low,
+				"close":                 v.Close,
+				"change":                v.Change,
+				"volume":                v.Volume,
+				"value":                 v.Value,
+				"frequency":             v.Frequency,
+				"index_individual":      v.IndexIndividual,
+				"offer":                 v.Offer,
+				"offer_volume":          v.OfferVolume,
+				"bid":                   v.Bid,
+				"bid_volume":            v.BidVolume,
+				"listed_shares":         v.ListedShares,
+				"tradeble_shares":       v.TradebleShares,
+				"weight_for_index":      v.WeightForIndex,
+				"foreign_sell":          v.ForeignSell,
+				"foreign_buy":           v.ForeignBuy,
+				"delisting_date":        v.DelistingDate,
+				"non_regular_volume":    v.NonRegularVolume,
+				"non_regular_value":     v.NonRegularValue,
+				"non_regular_frequency": v.NonRegularFrequency,
+				"last_modified":         v.LastModified,
+			}
+		}
+		return c.JSON(results)
 	}
 
 	return c.JSON(data)
